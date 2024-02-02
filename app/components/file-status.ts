dc9bcac0c47c9ddf70241280756b2ae5cddfc61c
0xADADA <0xadada.pub@0xadada.pub>
2024-02-02 10:27:13 -0500

    implemented <FileStatus />

---
+++ app/components/file-status.ts
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import titleize from 'titleize';
+import type { ScannedFile } from 'crouton-subtype/lib/data.json.d';
+
+export interface FileStatusSignature {
+  Args: {
+    file: ScannedFile;
+  }
+}
+
+export default class FileStatus extends Component<FileStatusSignature> {
+  get status() {
+    return titleize(this.args.file.status);
+  }
+
+  get isAvailable() {
+    return this.args.file.status === 'available';
+  }
+}
---
+++ app/lib/data.json.d.ts
@@ -0,0 +1,8 @@
+export type ScannedFile = {
+  name: string;
+  device: string;
+  path: string;
+  status: 'available' | 'scheduled';
+}
+
+export type ScannedFiles = Array<ScannedFile>;
